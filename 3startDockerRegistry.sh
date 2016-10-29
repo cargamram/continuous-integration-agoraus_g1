ddf2e7e875a4557064dcba453acba59cbf964e73
ManuelLR <manuellr.git@gmail.com>
2016-10-29 15:37:44 +0200

    Cambios básicos para que el sistema funcione correctamente Se han realizado los cambios básicos para que el sistema funcione correctamente como son la eliminación de todo lo relacionado con GitLab y la inclusión de las relacion con el contenedor de proxy inverso

--- 3startDockerRegistry.sh
+++ 3startDockerRegistry.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-PORT=5000
 VOLUME_PATH=~/docker-registry-data
-docker run -d -p $PORT:5000 --restart=always --name registry \
+REGISTRY_DOMAIN=registry.*
+docker run -d -e VIRTUAL_HOST=$REGISTRY_DOMAIN --restart=always --name registry \
 -v $VOLUME_PATH:/var/lib/registry \
   registry:2
 
--- 4startJenkins.sh
+++ 4startJenkins.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-JENKINS_PORT=8090
+JENKINS_DOMAIN=jenkins.*
 JENKINS_CONTAINER_NAME=jenkins
 JENKINS_HOME=~/jenkins_home
 
@@ -13,18 +13,20 @@ then
 	docker rm $JENKINS_CONTAINER_NAME
 fi
 
-echo "Starting jenkins container on port $JENKINS_PORT and jenkins home is $JENKINS_HOME"
+echo "Starting jenkins container on domain $JENKINS_DOMAIN and jenkins home is $JENKINS_HOME"
 # https://github.com/jenkinsci/docker
 # https://hub.docker.com/r/jenkinsci/jenkins/tags/
 # /var/jenkins_home contains all plugins and configuration
 docker run -d --name $JENKINS_CONTAINER_NAME \
-	-p $JENKINS_PORT:8080 -p 50000:50000 \
+	-e VIRTUAL_HOST=$JENKINS_DOMAIN \
+	-p 50000:50000 \
 	-v $JENKINS_HOME:/var/jenkins_home \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v $(which docker):/bin/docker \
 	-v /usr/lib/x86_64-linux-gnu/libapparmor.so.1.1.0:/lib/x86_64-linux-gnu/libapparmor.so.1 \
 	-u root \
-	jenkins:1.609.3
+	--restart=alway
+	jenkins:2.7.4
 
 #the last 3 volume bindings are important in order to enable jenkins to run docker, see 
 #http://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
