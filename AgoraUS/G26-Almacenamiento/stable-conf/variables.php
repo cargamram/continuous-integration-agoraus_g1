ca8c843a1d38f884fd87df6fe7065041d74c4855
Fermín Fernández Borrego <ferminffb@gmail.com>
2017-01-09 19:08:52 +0100

    Almacenamiento
    Añadidos los archivos de Almacenamiento

---
+++ AgoraUS/G26-Almacenamiento/stable-conf/variables.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @file
+* \brief Variables del sistema
+* \details Variables que se usarán en el subsistema: Título, tiempo de la cookie
+*  y credenciales de conexión a la base de datos.
+* \author auth.agoraUS
+*/
+    define("TITLE", "Agora@US");
+    define("ONE_YEAR", 3600*24*365);
+    //DATABASE
+    
+    //define("DB_HOST", "mysql:dbname=n260m_17280281_agoraus;host=sql210.260mb.net");
+    //define("DB_USER", "n260m_17280281");
+    //define("DB_PASS", "EGC1516");
+    
+   $servername = 'exdb';
+$username = 'user1';
+$pass = 'user';
+$dbname = 'egcdb';
+
+?>
